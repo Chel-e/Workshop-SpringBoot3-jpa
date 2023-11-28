04b80e79d34d168858fc3587718d2a2258fa31b8
Michel Ferreira <michel.mf628@gmail.com>
2023-11-27 21:03:27 -0300

    OrderStatus enum

--- src/main/java/com/demo/demo/entities/Order.java
+++ src/main/java/com/demo/demo/entities/Order.java
@@ -3,6 +3,7 @@ package com.demo.demo.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.demo.demo.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Column;
@@ -22,16 +23,25 @@ public class Order implements Serializable{
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     @Column
     private Instant moment;
+    @Column
+    private Integer orderStatus;
     @ManyToOne
     @JoinColumn (name = "client_id")
     private User client;
     
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus,User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);;
         this.client = client;
     }
     public Order() {
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
     }
     public User getUser() {
         return client;
@@ -42,14 +52,16 @@ public class Order implements Serializable{
     public Instant getMoment() {
         return moment;
     }
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.GetCode();
+        }
+    }
     public void setMoment(Instant moment) {
         this.moment = moment;
-    }
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
     }
     @Override
     public int hashCode() {
