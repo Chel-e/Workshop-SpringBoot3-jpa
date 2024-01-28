73ec53d79a8897eb303e89ac477ca32b92af5061
Michel Ferreira <michel.mf628@gmail.com>
2024-01-28 20:36:24 -0300

    Exception handling - update

--- src/main/java/com/demo/demo/services/UserService.java
+++ src/main/java/com/demo/demo/services/UserService.java
@@ -11,35 +11,49 @@ import com.demo.demo.entities.User;
 import com.demo.demo.repositories.UserRepository;
 import com.demo.demo.services.exceptions.DatabaseException;
 import com.demo.demo.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
     public List<User> findAll() {
         return userRepository.findAll();
     }
+
     public User findById(Long id) {
         Optional<User> result = userRepository.findById(id);
         return result.orElseThrow(() -> new ResourceNotFoundException(id));
     }
+
     public User insert(User obj) {
         return userRepository.save(obj);
     }
+
     public void delete(Long id) {
         try {
             userRepository.deleteById(id);
         } catch (DataIntegrityViolationException e) {
             throw new DatabaseException(e.getMessage());
         }
-    } 
-    public User update(Long id, User obj) {
-        if (userRepository.existsById(id)) {
-            User entity = userRepository.getReferenceById(id);
-            updateData(entity, obj);
-            return userRepository.save(entity);
-        }
-        return null;
     }
+
+    public User update(Long id, User obj) {
+        try {
+            if (userRepository.existsById(id)) {
+                User entity = userRepository.getReferenceById(id);
+                updateData(entity, obj);
+                return userRepository.save(entity);
+            } else {
+                throw new ResourceNotFoundException(id);
+            }
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
     private void updateData(User entity, User obj) {
         entity.setName(obj.getName());
         entity.setEmail(obj.getEmail());
